5a59527cbf7b57cc0d50de02efad6393867542e9
Kevin Allain <kevallain@gmail.com>
2024-07-17 19:28:21 +0100

    going to modify display of our platforms for clarity and readibility

--- text-analysis-frontend-next/pages/Individual.js
+++ text-analysis-frontend-next/pages/Individual.js
@@ -15,47 +15,34 @@ import plagiarism_data from "@/public/data/codechecker_plagiarism_example.json";
 export default function Individual(props) {
   console.log("Individual | props: ", props);
   console.log("Individual | localStorage: ", localStorage);
-  console.log(
-    "Individual | useContext(FormDataContext): ",
-    useContext(FormDataContext)
-  );
+  console.log( "Individual | useContext(FormDataContext): ", useContext(FormDataContext) );
   const { formData, setFormData } = useContext(FormDataContext);
   const colorWorking = "#728a8d"; // grey
   const colorLowRisk = "#3cc343"; // green
   const colorMediumRisk = "#d97826"; // orange
   const colorHighRisk = "#d2342d"; // red
 
+  //   TODO system to adapt for several users
+  const userFullName = "Alice Black";
+
   const testModules = [
     {
       module: "Programming in Java",
-      courseworks: [
-        { name: "Coursework 1", score_ai_detection: "X", score_plagiarism: 67, score_collusion: 28, color_ai_detection: colorWorking,           color_plagiarism: colorHighRisk,           color_collusion: colorLowRisk, },
-        { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: 9, score_collusion: "X", color_ai_detection: colorLowRisk,           color_plagiarism: colorLowRisk,           color_collusion: colorWorking,           },
-        { name: "Coursework 3", score_ai_detection: 49, score_plagiarism: 1, score_collusion: 8, color_ai_detection: colorMediumRisk,           color_plagiarism: colorLowRisk,           color_collusion: colorLowRisk,           },
-      ],
+      courseworks: [ { name: "Coursework 1", score_ai_detection: "X", score_plagiarism: 67, score_collusion: 28, color_ai_detection: colorWorking,           color_plagiarism: colorHighRisk,           color_collusion: colorLowRisk, }, { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: 9, score_collusion: "X", color_ai_detection: colorLowRisk,           color_plagiarism: colorLowRisk,           color_collusion: colorWorking,           }, { name: "Coursework 3", score_ai_detection: 49, score_plagiarism: 1, score_collusion: 8, color_ai_detection: colorMediumRisk,           color_plagiarism: colorLowRisk,           color_collusion: colorLowRisk,           }, ],
     },
     {
       module: "TypeScript 2024",
-      courseworks: [
-        { name: "Coursework 1", score_ai_detection: 44, score_plagiarism: 66, score_collusion: 98, color_ai_detection: colorMediumRisk, color_plagiarism: colorHighRisk,           color_collusion: colorHighRisk,           },
-        { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: 69, score_collusion: "X", color_ai_detection: colorLowRisk,           color_plagiarism: colorHighRisk,           color_collusion: colorWorking, },
-        { name: "Coursework 3", score_ai_detection: 49, score_plagiarism: 16, score_collusion: 8, color_ai_detection: colorMediumRisk,           color_plagiarism: colorLowRisk,           color_collusion: colorLowRisk, },
+      courseworks: [ { name: "Coursework 1", score_ai_detection: 44, score_plagiarism: 66, score_collusion: 98, color_ai_detection: colorMediumRisk, color_plagiarism: colorHighRisk,           color_collusion: colorHighRisk,           }, { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: 69, score_collusion: "X", color_ai_detection: colorLowRisk,           color_plagiarism: colorHighRisk,           color_collusion: colorWorking, }, { name: "Coursework 3", score_ai_detection: 49, score_plagiarism: 16, score_collusion: 8, color_ai_detection: colorMediumRisk,           color_plagiarism: colorLowRisk,           color_collusion: colorLowRisk, },
       ],
     },
     {
       module: "C# 2021",
-      courseworks: [
-        { name: "Coursework 1", score_ai_detection: "X", score_plagiarism: 66, score_collusion: 98, color_ai_detection: colorWorking,           color_plagiarism: colorHighRisk,           color_collusion: colorHighRisk, },
-        { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: 69, score_collusion: 38, color_ai_detection: colorLowRisk,           color_plagiarism: colorHighRisk,           color_collusion: colorMediumRisk, },
-        { name: "Coursework 3", score_ai_detection: "X", score_plagiarism: 16, score_collusion: 8, color_ai_detection: colorWorking,           color_plagiarism: colorLowRisk,           color_collusion: colorLowRisk, },
+      courseworks: [ { name: "Coursework 1", score_ai_detection: "X", score_plagiarism: 66, score_collusion: 98, color_ai_detection: colorWorking,           color_plagiarism: colorHighRisk,           color_collusion: colorHighRisk, }, { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: 69, score_collusion: 38, color_ai_detection: colorLowRisk,           color_plagiarism: colorHighRisk,           color_collusion: colorMediumRisk, }, { name: "Coursework 3", score_ai_detection: "X", score_plagiarism: 16, score_collusion: 8, color_ai_detection: colorWorking,           color_plagiarism: colorLowRisk,           color_collusion: colorLowRisk, },
       ],
     },
     {
       module: "Stock Prediction",
-      courseworks: [
-        { name: "Coursework 1", score_ai_detection: 44, score_plagiarism: 66, score_collusion: "X", color_ai_detection: colorMediumRisk,           color_plagiarism: colorHighRisk,           color_collusion: colorWorking, },
-        { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: "X", score_collusion: 38, color_ai_detection: colorLowRisk,           color_plagiarism: colorWorking,           color_collusion: colorMediumRisk, },
-        { name: "Coursework 3", score_ai_detection: 49, score_plagiarism: 16, score_collusion: "X", color_ai_detection: colorMediumRisk, color_plagiarism: colorMediumRisk,           color_collusion: colorWorking, },
+      courseworks: [ { name: "Coursework 1", score_ai_detection: 44, score_plagiarism: 66, score_collusion: "X", color_ai_detection: colorMediumRisk,           color_plagiarism: colorHighRisk,           color_collusion: colorWorking, }, { name: "Coursework 2", score_ai_detection: 14, score_plagiarism: "X", score_collusion: 38, color_ai_detection: colorLowRisk,           color_plagiarism: colorWorking,           color_collusion: colorMediumRisk, }, { name: "Coursework 3", score_ai_detection: 49, score_plagiarism: 16, score_collusion: "X", color_ai_detection: colorMediumRisk, color_plagiarism: colorMediumRisk,           color_collusion: colorWorking, },
       ],
     },
   ];
@@ -72,8 +59,8 @@ export default function Individual(props) {
         <Navbar />
         <div className="row">
           {/* <Sidebar /> */}
-          <div className="col-md-10">
-            <h3>Individual Scores</h3>
+          <div className="col-md-12">
+            <h3>{userFullName} - Individual Scores</h3>
             <h4>Modules History</h4>
             {testModules.map((a, index) => (
               <>
